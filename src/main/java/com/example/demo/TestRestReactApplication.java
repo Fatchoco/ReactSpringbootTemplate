c1d3a3bfbc835b57b7c5dd1afaf3d0e29bb2c7d4
Erico <erico.i.am@gmail.com>
2021-08-10 22:45:32 +0800

    Initial Commit

---
+++ src/main/java/com/example/demo/TestRestReactApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRestReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestRestReactApplication.class, args);
+	}
+
+}
